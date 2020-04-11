bec0b3c5dcad5718c82d5754038e1806488085d3
Sinan Dinar <sinandinar@protonmail.com>
2020-04-11 18:15:14 +0300

    Added Pug template engine

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/poker-planning-add-story-list'
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/poker-planning-add-story-list',
+    name: 'AddStoryList',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "scrum-master-planning" */ '../views/AddStoryList.vue')
   }
 ]
 
--- src/views/AddStoryList.vue
+++ src/views/AddStoryList.vue
@@ -1,8 +1,6 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div(class="home")
+    HelloWorld(msg="Welcome to Your Vue.js App")
 </template>
 
 <script>
@@ -10,7 +8,7 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: 'AddStoryList',
   components: {
     HelloWorld
   }
